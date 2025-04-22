c449403b71a3dc14773fd996bf39bd02b7ab802b
Raja kumar <rajaraja360parth@gmail.com>
2025-04-22 23:28:27 +0530

    updated cors

--- src/index.ts
+++ src/index.ts
@@ -28,7 +28,7 @@ app.use(express.json({ limit: '10kb' }));
 
 
 app.use(cors({
-    origin: ["http://localhost:5173", "http://192.168.29.207:5173","https://onebrain.vercel.app/"], // Add all your frontend URLs
+    origin: ["http://localhost:5173", "http://192.168.29.207:5173","https://onebrain.vercel.app"], // Add all your frontend URLs
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"]
